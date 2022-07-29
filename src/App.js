dce35ee47efad6ea71239d301889e1e11322aedc
iGorillaz <barannik.denis.92@gmail.com>
2022-07-30 02:18:57 +0300

    Project Init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Slider } from "./components/Slider/Slider";
+function App() {
+  return (
+    <div className="App">
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+export const initalState = [
+  { id: 1, name: "LTC" },
+  { id: 2, name: "DASH" },
+  { id: 3, name: "BTC" },
+  { id: 4, name: "USD" },
+  { id: 5, name: "ETH" },
+  { id: 6, name: "BCH" },
+];
